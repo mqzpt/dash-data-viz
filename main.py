3557a7b59da54c319f483dcf96866467fc032632
Matthew Athanasopoulos <matthew.athanasopoulos@gmail.com>
2024-09-18 01:34:23 -0400

    feat: create initial dash project

---
+++ main.py
@@ -0,0 +1,11 @@
+# Helloooooo
+
+import dash
+from dash import html
+
+app = dash.Dash(__name__)
+
+app.layout = html.P("Hello Dash World!")
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
